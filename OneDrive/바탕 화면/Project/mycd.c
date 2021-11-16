5a926f1816da73f8c69db47080e1aa7ea1f2a95a
Babpul <dhrbqls12@naver.com>
2021-11-16 11:57:56 +0900

    v1
    Signed-off-by: Babpul <dhrbqls12@naver.com>

--- OneDrive/바탕 화면/Project/mycd.c
+++ OneDrive/바탕 화면/Project/mycd.c
@@ -1,5 +1,36 @@
 #include "user.h"
+/*
+이름    : mycd 함수
+작성자  : 오규빈
+기능    : 작업 디렉토리의 경로를 변경한다
+받는값  : 경로 문자열
+리턴값  : X
+*/
+void mycd (const char* path){
+    if(path == NULL)
+    {//path 인자가 없을시
+        rear_dir_list_ptr == front_dir_list_ptr;//작업 디렉터리를 홈디렉터리로 설정
+    }
 
-void mycd (void){
-    
+    else if (path != NULL)
+    {
+        int i = 0;
+        int first = 0;
+        while(*(path+i) == '\0')
+        {
+          while(*(path+i) == '/')
+          {
+            if (first = 0)
+            {
+                first++;
+            }
+            else 
+            {
+                
+            }
+          } 
+        }
+           
+        
+    }
 }
